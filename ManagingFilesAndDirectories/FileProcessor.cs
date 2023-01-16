8b2d14a1eeb56f037f1d2c4396376c74084c6dfa
Sital Sitoula <heavywater221@gmail.com>
2023-01-16 00:41:49 -0600

    Add project files.

---
+++ ManagingFilesAndDirectories/FileProcessor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace ManagingFilesAndDirectories
+{
+	class FileProcessor
+	{
+		private const string BackupDirectoryName = "backup";
+		private const string InProgressDirectoryName = "processing";
+		private const string CompletedDirectoryName = "complete";
+
+		public string InputFilePath { get; }
+
+		public FileProcessor(string filePath) => InputFilePath = filePath;
+
+		public void Process()
+		{
+			WriteLine($"Begin process of {InputFilePath}");
+
+			// Check if file exists
+			if (!File.Exists(InputFilePath))
+			{
+				WriteLine($"ERROR: file {InputFilePath} does not exist.");
+				return;
+			}
+
+			string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+			WriteLine($"Root data path is {rootDirectoryPath}");
+
+			// Check if backup dir exists
+			string backupDirectoryPath = Path.Combine(rootDirectoryPath, BackupDirectoryName);
+
+			//if (!Directory.Exists(backupDirectoryPath))
+			//{
+			WriteLine($"Attempting to create {backupDirectoryPath}");
+			Directory.CreateDirectory(backupDirectoryPath);
+			//}
+
+			// Copy file to backup dir
+			string inputFileName = Path.GetFileName(InputFilePath);
+			string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+			WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+			File.Copy(InputFilePath, backupFilePath, true);
+
+			// Move to in progress dir
+			Directory.CreateDirectory(Path.Combine(rootDirectoryPath, InProgressDirectoryName));
+			string inProgressFilePath =
+				Path.Combine(rootDirectoryPath, InProgressDirectoryName, inputFileName);
+
+			if (File.Exists(inProgressFilePath))
+			{
+				WriteLine($"ERROR: a file with the name {inProgressFilePath} is already being processed.");
+				return;
+			}
+
+			WriteLine($"Moving {InputFilePath} to {inProgressFilePath}");
+			File.Move(InputFilePath, inProgressFilePath);
+
+
+			// Determine type of file
+			string extension = Path.GetExtension(InputFilePath);
+
+			switch (extension)
+			{
+				case ".txt":
+					ProcessTextFile(inProgressFilePath);
+					break;
+				default:
+					WriteLine($"{extension} is an unsupported file type.");
+					break;
+			}
+
+			// Move file after processing is complete
+			string completedDirectoryPath = Path.Combine(rootDirectoryPath, CompletedDirectoryName);
+			Directory.CreateDirectory(completedDirectoryPath);
+			WriteLine($"Moving {inProgressFilePath} to {completedDirectoryPath}");
+			//File.Move(inProgressFilePath, Path.Combine(completedDirectoryPath, inputFileName));
+
+			string completedFileName =
+				$"{Path.GetFileNameWithoutExtension(InputFilePath)}-{Guid.NewGuid()}{extension}";
+
+			//completedFileName = Path.ChangeExtension(completedFileName, ".complete");
+
+			var completedFilePath = Path.Combine(completedDirectoryPath, completedFileName);
+
+			File.Move(inProgressFilePath, completedFilePath);
+
+			string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
+			Directory.Delete(inProgressDirectoryPath, true);
+		}
+
+		private void ProcessTextFile(string inProgressFilePath)
+		{
+			WriteLine($"Processing text file {inProgressFilePath}");
+			// Read in and process
+		}
+	}
+}
---
+++ ManagingFilesAndDirectories/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using static System.Console;
+
+namespace ManagingFilesAndDirectories
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine("Parsing command line options");
+			var command = args[0];
+
+			if (command == "--file")
+			{
+				var filePath = args[1];
+
+				// check if the file path is absolute
+				if (!Path.IsPathFullyQualified(filePath))
+				{
+					WriteLine($"Error: path '{filePath}' must be fully qualified");
+					ReadLine();
+					return;
+				}
+
+				WriteLine($"Single file {filePath} selected");
+				ProcessFiles(filePath);
+			}
+			else if (command == "--dir")
+			{
+				var directoryPath = args[1];
+				var fileType = args[2];
+				WriteLine($"Directory {directoryPath} selected for {fileType} files");
+				ProcessDirectory(directoryPath, fileType);
+			}
+			else
+			{
+				WriteLine("Invalid Command Line options");
+			}
+
+			WriteLine("Press Enter to quit");
+			ReadLine();
+		}
+
+		private static void ProcessDirectory(string directoryPath, string fileType)
+		{
+			switch (fileType)
+			{
+				case "TEXT":
+					string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+					foreach (string textFilePath in textFiles)
+					{
+						new FileProcessor(textFilePath).Process();
+					}
+					break;
+				default:
+					WriteLine($"Error: {fileType} is not supported");
+					return;
+			}
+
+
+		}
+		private static void ProcessFiles(string filePath)
+		{
+			var fileProcessor = new FileProcessor(filePath);
+			fileProcessor.Process();
+		}
+	}
+}
