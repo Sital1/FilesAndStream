fbdc2ee1da8c455e80eff439c71886008d43c875
Sital Sitoula <heavywater221@gmail.com>
2023-01-17 01:10:43 -0600

    Approval Tests

--- ManagingFiles.Tests/TextFileProcessorShould.cs
+++ ManagingFiles.Tests/TextFileProcessorShould.cs
@@ -1,5 +1,7 @@
 using ManagingFilesAndDirectories;
 using System.IO.Abstractions.TestingHelpers;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 
 namespace ManagingFiles.Tests
 {
@@ -36,6 +38,39 @@ namespace ManagingFiles.Tests
 			Assert.Equal("Line 1", lines[0]);
 			Assert.Equal("LINE 2", lines[1]);
 			Assert.Equal("Line 3", lines[2]);
+		}
+
+		[Fact]
+		[UseReporter(typeof(DiffReporter))]
+		public void MakeSecondLineUpperCase_ApprovalTests()
+		{
+			// create a mock input file
+			var mockInputFile = new MockFileData("Line 1\nLine 2\nLine 3");
+
+			// set mock file system starting state
+			var mockFileSystem = new MockFileSystem();
+			mockFileSystem.AddFile(@"c:\root\in\myfile.txt", mockInputFile);
+			mockFileSystem.AddDirectory(@"c:\root\out");
+
+			// Create TextFileProcessor with mock file system
+			var sut = new TextFileProcessor(@"c:\root\in\myfile.txt", @"c:\root\out\myfile.txt", mockFileSystem);
+
+			// Process test file
+			sut.Process();
+
+			// Check mock file system for output file
+			Assert.True(mockFileSystem.FileExists(@"c:\root\out\myfile.txt"));
+
+			// Check content of the output file
+			MockFileData processedFile = mockFileSystem.GetFile(@"c:\root\out\myfile.txt");
+
+			string[] lines = processedFile.TextContents.Split(
+					new string[] { "\r\n", "\r", "\n" },
+					StringSplitOptions.None
+				);
+
+			// approval
+			Approvals.Verify(processedFile.TextContents);
 		}
 	}
 }
